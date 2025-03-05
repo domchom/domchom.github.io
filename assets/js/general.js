bd0bd46033fc24d3a6d9da99dbc43563ef7b3a03
domchom <67873793+domchom@users.noreply.github.com>
2025-03-05 13:11:41 -0600

    further update nav

--- assets/js/general.js
+++ assets/js/general.js
@@ -14,13 +14,14 @@ document.addEventListener("DOMContentLoaded", function() {
             const menu = document.querySelector('.left-menu');
             menu.innerHTML = data;
 
-            // Get the current page URL
+            // Get the current page URL (e.g., "index.html", "aboutme.html")
             const currentPage = window.location.pathname.split('/').pop();
 
             // Find the link corresponding to the current page and apply styles
             const links = menu.querySelectorAll('a');
             links.forEach(link => {
-                if (link.getAttribute('href') === currentPage) {
+                const linkPage = link.getAttribute('href').split('/').pop(); // Get filename from href
+                if (linkPage === currentPage) {
                     link.style.fontWeight = 'bold';
                     link.style.color = 'green';
                 }
