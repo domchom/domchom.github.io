3ed6c82760255b2012d5dfa00f0b8b005605ad7d
domchom <67873793+domchom@users.noreply.github.com>
2025-03-05 15:24:45 -0600

    update some hover

--- assets/js/midwest_auto.js
+++ assets/js/midwest_auto.js
@@ -10,19 +10,19 @@ const imageFiles = [
     //"12_IMG_9148-Pano-positive.jpg",
     "12_IMG_9157-Pano-positive.jpg",
     "12_IMG_9166-Pano-positive.jpg",
-    "12_IMG_9184-Pano-positive.jpg",
     //"12_IMG_9185-Pano-positive.jpg",
     "12_IMG_8780-Pano-positive.jpg",
-    "12_IMG_9326-Pano-positive.jpg",
     "12_IMG_9338-Pano-positive.jpg",
     "240905-2_raw0001-positive.jpg",
     "240905-2_raw0032-positive.jpg",
-    "240929-1_raw0052-positive.jpg",
     "240929-1_raw0060-positive.jpg",
     "240929-1_raw0061-positive.jpg",
     "240929-1_raw0062-positive.jpg",
     "241013-1_raw0012-positive.jpg",
     "12_IMG_7476-positive.jpg",
+    "12_IMG_9184-Pano-positive.jpg",
+    "240929-1_raw0052-positive.jpg",
+    "12_IMG_9326-Pano-positive.jpg",
 ];
 
 const basePath = "assets/images/midwest_auto/";
